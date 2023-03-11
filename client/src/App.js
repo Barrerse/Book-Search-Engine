f50762e378cc1f4fba8fc74e7872d835840643db
Barrerse <sebastianbarrera@live.com>
2023-03-11 18:49:01 -0500

    changed the apollo client in app.js to try to fix errors. kept old one in comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,74 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider, InMemmoryCache, ApolloClient } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
+import { InMemoryCache } from '@apollo/client/cache';
+import { ApolloClient } from '@apollo/client/core';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token');
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-  },
   uri: '/graphql',
-  cache: new InMemmoryCache(),
+  cache: new InMemoryCache(),
 });
 
 function App() {
   return (
-    
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { ApolloProvider, InMemmoryCache, ApolloClient } from '@apollo/react-hooks';
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
+// import Navbar from './components/Navbar';
+// import ApolloClient from 'apollo-boost';
+
+// const client = new ApolloClient({
+//   request: (operation) => {
+//     const token = localStorage.getItem('id_token');
+
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : '',
+//       },
+//     });
+//   },
+//   uri: '/graphql',
+//   cache: new InMemmoryCache(),
+// });
+
+// function App() {
+//   return (
+    
+//     <ApolloProvider client={client}>
+//     <Router>
+//       <>
+//         <Navbar />
+//         <Switch>
+//           <Route exact path='/' component={SearchBooks} />
+//           <Route exact path='/saved' component={SavedBooks} />
+//           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+//         </Switch>
+//       </>
+//     </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
