88a20c65eef85586f5966489b49c554af17864b5
Barrerse <sebastianbarrera@live.com>
2023-03-11 19:07:54 -0500

    edited savedbooks and searchbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -7,9 +7,10 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/react-hooks'; // added
+import { SAVE_BOOK } from '../utils/mutations'; // added
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -59,6 +60,9 @@ const SearchBooks = () => {
     }
   };
 
+  // add useMutation hook to execute the SAVE_BOOK mutation
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -72,11 +76,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
@@ -84,6 +86,8 @@ const SearchBooks = () => {
       console.error(err);
     }
   };
+
+
 
   return (
     <>
